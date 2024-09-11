580b9ff765f824ad24098dcd941c22a8f1ed57d8
PierreLouis94 <pierre.bonvin@gmx.de>
2024-09-11 10:01:00 +0200

    docker fix

--- docker/scripts/build_server.sh
+++ docker/scripts/build_server.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 CURRENT_DIR=`dirname "$0"`
 
-# Since Mac doesn't come with realpath by default, let's set the full
-# paths using PWD.
-pushd .
 cd $CURRENT_DIR/..
-DEPLOY_DIR=$PWD
-cd $CURRENT_DIR/../../..
-ROOT_DIR=$PWD
-popd
+DEPLOY_DIR="$PWD"
+cd ..
+ROOT_DIR="$PWD"
 
-docker run --rm --gpus all \
-    -e TRAME_BUILD_ONLY=1 \
+docker run --rm          \
+    -e TRAME_CLIENT_TYPE=vue2 \
     -v "$DEPLOY_DIR:/deploy" \
-    -v "$ROOT_DIR/paraview-visualizer:/local-app" \
-    kitware/trame:1.2-glvnd-runtime-ubuntu20.04-py39
+    -v "$ROOT_DIR:/local-app"  \
+    kitware/trame:py3.10-glvnd build
